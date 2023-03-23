fd046b63953367040516d2335fb9b86192b867b8
Mrinmoy Ghosh <mrinmoygh081@gmail.com>
2023-03-23 17:20:57 +0530

    mern-todo-frontend

---
+++ src/components/Task.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaEdit, FaCheckDouble, FaRegTrashAlt } from "react-icons/fa";
+
+const Task = ({ data, index, deleteTask, getSingleTask, setToComplete }) => {
+  return (
+    <>
+      <div className={data.isCompleted ? "task completed" : "task"}>
+        <p>
+          <b>{index + 1}.</b> {data?.name}
+        </p>
+        <div className="task-icons">
+          <FaCheckDouble
+            color="green"
+            onClick={() => setToComplete(data._id)}
+          />
+          <FaEdit color="purple" onClick={() => getSingleTask(data)} />
+          <FaRegTrashAlt color="red" onClick={() => deleteTask(data._id)} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Task;
